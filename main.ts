a467cd2a625eb4df90fab9b1f3f8a294317d17c3
nono38 <74985866+nono38@users.noreply.github.com>
2022-08-26 20:40:03 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,36 @@
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P13, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P13, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
     radio.sendNumber(3)
 })
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MES_DPAD_BUTTON_B_UP, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
     radio.sendNumber(0)
-    basic.showString("up")
 })
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MES_DPAD_BUTTON_A_UP, function () {
-    radio.sendNumber(0)
-    basic.showString("up")
-})
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P14, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    radio.sendNumber(2)
-})
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P16, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P16, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
     radio.sendNumber(4)
 })
-control.onEvent(EventBusSource.MICROBIT_ID_IO_P15, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
-    radio.sendNumber(1)
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P14, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
+    radio.sendNumber(2)
 })
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MES_DPAD_BUTTON_B_DOWN, function () {
-    radio.sendNumber(100)
-    basic.showString("down")
-})
-control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MES_DPAD_BUTTON_A_DOWN, function () {
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_A, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
     radio.sendNumber(-100)
-    basic.showString("down")
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_UP, function () {
+    radio.sendNumber(0)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_BUTTON_B, EventBusValue.MICROBIT_BUTTON_EVT_DOWN, function () {
+    radio.sendNumber(100)
+})
+control.onEvent(EventBusSource.MICROBIT_ID_IO_P15, EventBusValue.MICROBIT_PIN_EVT_FALL, function () {
+    radio.sendNumber(1)
 })
 radio.setGroup(1)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P5, PinPullMode.PullUp)
-pins.setPull(DigitalPin.P11, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+pins.setEvents(DigitalPin.P14, PinEventType.Edge)
+pins.setEvents(DigitalPin.P15, PinEventType.Edge)
+pins.setEvents(DigitalPin.P16, PinEventType.Edge)
+pins.setEvents(DigitalPin.P13, PinEventType.Edge)
 basic.forever(function () {
 	
 })
